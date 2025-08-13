8e847021f768ffcf44fa8b215147a6e65b196b52
Andrei Dragomir <dandrei.web@gmail.com>
2025-08-13 20:21:20 +0200

    Installed prettier

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -21,28 +21,29 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-const dbConnect = async ():Promise<Mongoose> => {
+const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "devflow",
-    })
-    .then((result) => {
-      console.log("Connected to MongoDB");
-      return result;
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB", error);
-      throw error;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        console.log("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB", error);
+        throw error;
+      });
   }
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
 
 export default dbConnect;
